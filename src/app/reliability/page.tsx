5e48f6388e75897383d53da3ff0359d511ce2146
javiseeker <javieralonso123@outlook.es>
2024-04-26 16:30:15 -0500

    initial commit

---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,6 @@
+import reliabilityImg from 'public/reliability.jpg';
+import Hero from '@/components/hero';
+
+export default function ReliabilityPage() {
+  return(<Hero imgData={reliabilityImg} imgAlt="security mask" title="Super high reliability host page!" ></Hero>);
+}
---
+++ src/app/scale/page.tsx
@@ -0,0 +1,6 @@
+import scaleImg from 'public/scale.jpg';
+import Hero from '@/components/hero';
+
+export default function ScalePage() {
+  return(<Hero imgData={scaleImg} imgAlt="heights" title="Scaling to outer heights!" ></Hero>);
+}
